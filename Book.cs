467528d4e21b477d0ce1315e20d1510bb26c0952
MikkelCH <mikk320i@edu.zealand.dk>
2023-10-05 14:32:34 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,48 @@
+﻿namespace BookLib
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Titel { get; set; }
+        public double Price { get; set; }
+
+        public Book(int id, string titel, double price)
+        {
+            Id = id;
+            Titel = titel;
+            Price = price;
+        }
+
+        public void ValidateTitel()
+        {
+            if (Titel == null)
+            {
+                throw new ArgumentNullException("Titel is null");
+            }
+
+            if (Titel.Length < 3)
+            {
+                throw new ArgumentOutOfRangeException("Titel must be at least 3 characters");
+            }
+        }
+
+        public void ValidatePrice()
+        {
+            if (Price < 0 || Price > 1200)
+            {
+                throw new ArgumentOutOfRangeException("Price was outofrange");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Titel = {Titel};  Price = {Price};";
+        }
+
+        public void Validate()
+        {
+            ValidateTitel();
+            ValidatePrice();
+        }
+    }
+}
---
+++ BooksRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLib
+{
+    public class BooksRepository
+    {
+        private int nextId = 1;
+        private List<Book> Books = new List<Book>();
+
+        public BooksRepository()
+        {
+            Books.Add(new Book(nextId++, "HarryPotter", 700.00));
+            Books.Add(new Book(nextId++, "2. Verdenskrig", 500.00));
+            Books.Add(new Book(nextId++, "Den kolde krig", 450.00));
+            Books.Add(new Book(nextId++, "Programming for dummies", 900.00));
+            Books.Add(new Book(nextId++, "How to build a table", 50.00));
+        }
+
+        public List<Book> Get(int? priceBelow=null, string? OrderBy=null)
+        {
+            IEnumerable<Book> result = new List<Book>(Books);
+            if (priceBelow != null) 
+            {
+                result.Where(book => book.Price < priceBelow);
+            }
+            if (OrderBy != null)
+            {
+                result = OrderBy switch
+                {
+                    "Titel" => result.OrderBy(book => book.Titel),
+                    "PriceDesc" => result.OrderByDescending(book => book.Price),
+                    "Price" => result.OrderBy(book => book.Price),
+                    _ => throw new ArgumentException("Invalid Orderby")
+                };
+            }
+
+            return result.ToList();
+        }
+
+        public Book? GetById(int id)
+        {
+            return Books.FirstOrDefault(a => a.Id == id);
+        }
+
+        public Book Add(Book book)
+        {
+            book.Validate();
+            book.Id = nextId++;
+            Books.Add(book);
+            return book;
+        }
+
+        public Book? Delete(int id)
+        {
+            Book? book = GetById(id);
+            if (book != null)
+            {
+                Books.Remove(book);
+                return(book);
+            }
+            return null;
+        }
+
+        public Book? Update(int id,  Book book)
+        {
+            book.Validate();
+            Book? existingBook = GetById(id);
+            if (existingBook != null)
+            {
+                existingBook.Titel = book.Titel;
+                existingBook.Price = book.Price;
+            }
+            return existingBook;
+
+        }
+    }
+}
